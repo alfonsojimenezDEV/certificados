7828b2d2703299a1e133611a16904eb4dcb412a2
Alfonso Jiménez <aljimeruz@gmail.com>
2022-02-10 13:45:12 +0100

    2º: Certificado

--- src/Certificado.svelte
+++ src/Certificado.svelte
@@ -5,13 +5,24 @@
     export let certificado;
 
     let modulos = [];
+    let nombreModulo = '';
     $:  modulos = Object.values(certificado.modulos);
     
+    function addModulo() {
+        console.log("añadir módulo");
+        
+    }
+
     console.log(certificado);
 </script>
 
 <div class="certificado">
     <h2>{certificado.name}</h2>
+    <form action="" on:submit|preventDefault={addModulo}>
+        <label for="">
+            <input type="text" placeholder="Nombre del módulo" bind:value={nombreModulo} /> <button>Add</button>
+        </label>
+    </form>
     {#each modulos as modulo}
         <Modulo bind:modulo/>
     {/each}
